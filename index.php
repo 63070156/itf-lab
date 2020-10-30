dc948e20d39e01bc305c459620011ef9420ab149
63070156 <73683369+63070156@users.noreply.github.com>
2020-10-30 12:45:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <html>
     <head>
+        <meta charset="UTF-8">
         <title>Hello world</title>
     </head>
     <body>
         <h1>My First Heading</h1>
         <p>My First Paragraph</p>
+        <p>My name is Wannasa Chonchoochart</p>
+        <p>My nickname is Fah</p>
+        <p>เรื่องตลก"ประเทศเเคนาดา"</p>
+        <p>เมื่อตอนองค์การนาซา (NASA) ทำการเตรียมตัวที่จะส่งมนุษย์อวกาศขึ้นไปบนอวกาศเป็นครั้งเเรก</p>
+        <p>พวกเขาพบว่าปากกาลูกลื่นธรรมดาไม่สามารถใช้งานในสุญญากาศได้</p>
+        <p>พวกเขาจึงจำเป็นต้องลงทุนกว่า 12 ล้านล้านดอลลาร์เเละเวลาอีกกว่าสิบปีในการคิดค้นปากกาที่สามารถใช้เขียนในสุญญากาศได้</p>
+        <p>ส่วนประเทศรัสเซียส่งมนุษย์อวกาศขึ้นไปบนอวกาศพร้อมกับดินสอเเท่งเดียว</p>
         <img src="cat.jpg">
+        <img src="cat1.jpg">
+        <img src="cat2.jpg">
         <br>
-        <a href="http://it.kmitl.ac.th">This is a link</a>
+        <a href="https://www.youtube.com/">This my favorite link</a>
 
         <h2>The unordered list</h2>
         <ul>
