ec69b4d519a9313dd553c8b00a126d11a5a6c880
63070156 <73683369+63070156@users.noreply.github.com>
2020-10-30 12:49:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
         <p>My First Paragraph</p>
         <p>My name is Wannasa Chonchoochart</p>
         <p>My nickname is Fah</p>
+        <a href="https://www.youtube.com/">This my favorite link</a>
         <p>เรื่องตลก"ประเทศเเคนาดา"</p>
         <p>เมื่อตอนองค์การนาซา (NASA) ทำการเตรียมตัวที่จะส่งมนุษย์อวกาศขึ้นไปบนอวกาศเป็นครั้งเเรก</p>
         <p>พวกเขาพบว่าปากกาลูกลื่นธรรมดาไม่สามารถใช้งานในสุญญากาศได้</p>
         <p>พวกเขาจึงจำเป็นต้องลงทุนกว่า 12 ล้านล้านดอลลาร์เเละเวลาอีกกว่าสิบปีในการคิดค้นปากกาที่สามารถใช้เขียนในสุญญากาศได้</p>
         <p>ส่วนประเทศรัสเซียส่งมนุษย์อวกาศขึ้นไปบนอวกาศพร้อมกับดินสอเเท่งเดียว</p>
-        <a href="https://www.youtube.com/">This my favorite link</a>
         <img src="cat.jpg">
         <img src="cat1.jpg">
         <img src="cat2.jpg">
