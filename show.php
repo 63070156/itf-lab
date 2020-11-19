c0daeb8ba9c484f84c058996a39b3f8641b39992
63070156 <73683369+63070156@users.noreply.github.com>
2020-11-19 20:37:46 +0700

    Update show.php

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'wannasa44.mysql.database.azure.com', 'fahfiily@wannasa44', 'Wannasa44', 'itflab', 3306);
+mysqli_real_connect($conn, 'wannasa44.mysql.database.azure.com', 'fahfiily@wannasa44', 'Wannasa441810', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
