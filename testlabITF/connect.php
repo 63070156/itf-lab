da28ac756961400af7a7fa6ebc6df493b014b0de
63070156 <73683369+63070156@users.noreply.github.com>
2020-11-27 12:11:07 +0700

    Update connect.php

--- testlabITF/connect.php
+++ testlabITF/connect.php
@@ -1,6 +1,6 @@
 <?php
     $conn = mysqli_init();
-    mysqli_real_connect($conn, 'wannasa.mysql.database.azure.com', 'wannasa@wannasa', 'Fah12345', 'datainsert', 3306);
+    mysqli_real_connect($conn, 'wannasa.mysql.database.azure.com', 'wannasa@wannasa', 'Fah12345', 'testitf', 3306);
     if(mysqli_connect_errno($conn)){
         echo "connect fail".mysqli_connect_error();
     }
